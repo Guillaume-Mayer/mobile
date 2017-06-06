086708ffa8f7ed65140d06cbd72cf95f7d77a654
Guillaume Mayer <gmayer@arkho.tech>
2017-06-06 18:27:38 -0400

    Added Firebase

--- poc/components/User.js
+++ poc/components/User.js
@@ -3,10 +3,26 @@ import {View, Image, Text} from 'react-native';
 
 export default class User extends Component {
 
+  getImageUri(name) {
+    if (name === 'Guillaume') {
+      return 'https://firebasestorage.googleapis.com/v0/b/reactnativearkhotest.appspot.com/o/guillaume.png?alt=media&token=d6cfc1c4-00ce-487b-a37f-77635fd74c3f';
+    } else if (name === 'Andres') {
+      return 'https://firebasestorage.googleapis.com/v0/b/reactnativearkhotest.appspot.com/o/andres.png?alt=media&token=a919f7b0-7c5a-466a-ae6c-4276ea508f76';
+    } else if (name === 'Jimmy') {
+      return 'https://firebasestorage.googleapis.com/v0/b/reactnativearkhotest.appspot.com/o/jimmy.png?alt=media&token=21abae5e-72aa-439a-af4e-b7e463709bf1';
+    } else if (name === 'Hector') {
+      return 'https://firebasestorage.googleapis.com/v0/b/reactnativearkhotest.appspot.com/o/hector.png?alt=media&token=77bcc614-b3c6-4adb-9df9-544895708c1e';
+    } else if (name === 'Marcelo') {
+      return 'https://firebasestorage.googleapis.com/v0/b/reactnativearkhotest.appspot.com/o/marcelo.png?alt=media&token=89a6eeda-d368-4c4a-a8f8-a0255cd415b2';
+    } else {
+      return 'https://firebasestorage.googleapis.com/v0/b/reactnativearkhotest.appspot.com/o/raul.png?alt=media&token=77c57ae9-8994-4be4-9f74-02acab1a1289';
+    }
+  }
+
   render() {
     return (
         <View style={{flex: 1, flexDirection: 'row'}}>
-          <Image style={{width: 30, height: 30}} source={require('../resources/images/Guillaume.png')}></Image>
+          <Image style={{width: 30, height: 30}} source={{uri: this.getImageUri(this.props.name)}}></Image>
           <Text style={{fontSize: 20}}>{this.props.name}</Text>
         </View>
     );
--- poc/index.ios.js
+++ poc/index.ios.js
@@ -8,11 +8,29 @@ import {
 import UserList from './components/UserList';
 import Map from './components/Map';
 
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyA_8o8GD_QUrHfsvpkTlgEWDoOwWQshanE",
+    authDomain: "reactnativearkhotest.firebaseapp.com",
+    databaseURL: "https://reactnativearkhotest.firebaseio.com",
+    projectId: "reactnativearkhotest",
+    storageBucket: "reactnativearkhotest.appspot.com",
+    messagingSenderId: "503266762160"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
 export default class poc extends Component {
   state = {
     selectedTab: 'users'
   };
   render() {
+    var fbase = firebaseApp.database();
+    fbase.ref('users/gmayer').once('value').then((item) =>{
+      console.log("FIREBASE: " + item.val().name);
+      console.log("FIREBASE: " + item.val().age);
+    });
     return (
       <View style={{flex: 1}}>
         <View style={{height: 20}}/>
