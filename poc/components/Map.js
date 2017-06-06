d5cd06133090e89bc139703d709ea3fdbfee789c
Guillaume Mayer <gmayer@arkho.tech>
2017-06-06 12:01:09 -0400

    Added IOS Tab

---
+++ poc/components/Map.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+//import MapView from 'react-native-maps';
+
+export default class Map extends Component {
+
+  render() {
+    return (
+        <Text>Mapa</Text>
+    );
+  }
+
+}
--- poc/index.ios.js
+++ poc/index.ios.js
@@ -3,16 +3,38 @@ import {
   AppRegistry,
   StyleSheet,
   View,
+  TabBarIOS,
 } from 'react-native';
 import UserList from './components/UserList';
+import Map from './components/Map';
 
 export default class poc extends Component {
+  state = {
+    selectedTab: 'users'
+  };
   render() {
     return (
-      <View style={styles.container}>
+      <View style={{flex: 1}}>
         <View style={{height: 20}}/>
-        <UserList/>
-      </View>
+        <TabBarIOS>
+          <TabBarIOS.Item
+            title='Usuarios'
+            selected={this.state.selectedTab === 'users'}
+            onPress={()=>{
+              this.setState({selectedTab: 'users'});
+            }}>
+            <UserList/>
+          </TabBarIOS.Item>
+          <TabBarIOS.Item
+            title='Mapa'
+            selected={this.state.selectedTab === 'map'}
+            onPress={()=>{
+              this.setState({selectedTab: 'map'});
+            }}>
+            <Map/>
+          </TabBarIOS.Item>
+        </TabBarIOS>
+     </View>
     );
   }
 }
