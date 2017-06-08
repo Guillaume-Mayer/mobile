596085c5a5188e45bbc5821b9169483b7f25e2fd
Guillaume Mayer <gmayer@arkho.tech>
2017-06-08 19:50:45 -0400

    Geo location

--- poc/components/Map.js
+++ poc/components/Map.js
@@ -7,10 +7,16 @@ export default class Map extends Component {
 
   constructor(props) {
     super(props);
-    this.markersRef = firebaseApp.database().ref('markers');
     this.state = {
+      region: {
+        latitude: -33.4489,
+        longitude: -70.6693,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1
+      },
       markers: []
     };
+    this.markersRef = firebaseApp.database().ref('markers');
   }
 
   listenForMarkers(markersRef) {
@@ -27,44 +33,76 @@ export default class Map extends Component {
           }
         });
       });
-      this.setState({markers: markers});
+      this.setState({region: this.state.region, markers: markers});
     });
   }
 
-  componentDidMount() {
+  componentWillMount() {
+    console.log("COMPONENT WILL MOUNT");
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log("POSITION OK: " + JSON.stringify(position));
+        /*this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          },
+          markers: this.state.markers
+        });*/
+      },
+      (error) => {
+        console.log("POSITION ERROR: " + JSON.stringify(error));
+      },
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
     this.listenForMarkers(this.markersRef);
   }
 
-  addMarker(markersRef) {
+  componentDidMount() {
+    console.log("COMPONENT DID MOUNT");
+  }
+
+  addMarker(markersRef, title, desc, lat, lng) {
+    var newMarkerRef = markersRef.push();
+    newMarkerRef.set({
+      title: title,
+      description: desc,
+      coords: {
+        latitude: lat,
+        longitude: lng
+      }
+    });
+  }
+
+  addMarkers(markersRef) {
     for (var i = 0; i < 10; i++) {
-      var newMarkerRef = this.markersRef.push();
-      newMarkerRef.set({
-        title: 'test' + i,
-        description: 'This is test ' + i,
-        coords: {
-          latitude: -33.4489 + (Math.random()*0.2) - 0.1,
-          longitude: -70.6693 + (Math.random()*0.2) - 0.1
-        }
-      });
+      this.addMarker(
+        markersRef, 
+        'test' + i, 
+        'This is test ' + i, 
+        this.state.region.latitude + (Math.random()*this.state.region.latitudeDelta) - (this.state.region.latitudeDelta/2), 
+        this.state.region.longitude + (Math.random()*this.state.region.longitudeDelta) - (this.state.region.longitudeDelta/2)
+        );
     }
   }
 
-  onRegionChangeComplete(region)  {
-    console.log("NEW REGION: " + region.latitude + ";" + region.longitude);
+  onRegionChangeComplete(region) {
+    console.log("REGION CHANGE COMPLETE: " + region.latitude + ";" + region.longitude);
   };
 
   render() {
+    console.log("RENDER");
     return (
       <View style={styles.container}> 
         <MapView
           style={styles.map}
-          initialRegion={{
-            latitude: -33.4489,
-            longitude: -70.6693,
-            latitudeDelta: 1,
-            longitudeDelta: 1,
-          }}
+          region={this.state.region}
+          onRegionChange={region => this.setState({region: region, markers: this.state.markers})}
           onRegionChangeComplete={this.onRegionChangeComplete}
+          onPress={e => this.addMarker(this.markersRef, 'test', 'tap test', e.nativeEvent.coordinate.latitude, e.nativeEvent.coordinate.longitude)}
+          showsUserLocation={true}
+          showsTraffic={true}
+          showsMyLocationButton={true}
         >
         {this.state.markers.map(marker => (
           <MapView.Marker
@@ -84,7 +122,7 @@ export default class Map extends Component {
         <Button
           title='Add 10 Markers'
           color='red'
-          onPress={() => this.addMarker(this.markersRef)}/>
+          onPress={() => this.addMarkers(this.markersRef)}/>
       </View>     
     );
   }
